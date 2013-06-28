970ceee8f24255d8a11d86b174a39b4bf5efae33
gabalese <gabriele@alese.it>
2013-06-28 12:06:29 +0200

    Added static route

--- MainHandlers.py
+++ MainHandlers.py
@@ -1,17 +1,19 @@
 import tornado.web
 from utils import Metadata
 
+
 class ErrorHandler(tornado.web.RequestHandler):
     def __init__(self, application, request, status_code):
         tornado.web.RequestHandler.__init__(self, application, request)
         self.set_status(status_code)
-    
+
     def write_error(self, status, **kwargs):
         self.write("Nope.")
-    
+
     def prepare(self):
         raise tornado.web.HTTPError(self._status_code)
-    
+
+
 class GetInfo(tornado.web.RequestHandler):
     def get(self, filename):
         if filename:
