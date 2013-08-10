6f729a7c308825066de0ad1561e862285c788f25
gabalese <gabriele@alese.it>
2013-08-10 13:37:42 +0200

    Added writetodisk method, completed addItem, working close method with mode="a" or "w"

--- epub/__init__.py
+++ epub/__init__.py
@@ -30,6 +30,11 @@ class EPUB(ZIP.ZipFile):
     def __init__(self, filename, mode="r"):
         """
         Global Init Switch
+
+        :type filename: str or StringIO
+        :param filename: File to be processed
+        :type mode: str
+        :param mode: "w" or "r", mode to init the zipfile
         """
         if mode == "w":
             assert not os.path.exists(filename), "Can't overwrite existing file: %s" % filename
@@ -45,7 +50,10 @@ class EPUB(ZIP.ZipFile):
 
     def __init__read(self, filename):
         """
-        Init if read file
+        Constructor to initialize the zipfile in read-only mode
+
+        :type filename: str or StringIO
+        :param filename: File to be processed
         """
         try:
             # Read the container
@@ -87,45 +95,46 @@ class EPUB(ZIP.ZipFile):
         except AttributeError:
             # It's a facultative field, after all
             coverid = None
+        self.cover = coverid  # This is the manifest ID of the cover
 
-        # This is the manifest ID of the cover
-        self.cover = coverid
-
-        self.info["manifest"] = [{"id": x.get("id"),
+        self.info["manifest"] = [{"id": x.get("id"),                # Build a list of manifest items
                                   "href": x.get("href"),
                                   "mimetype": x.get("media-type")}
                                  for x in self.opf.find("{0}manifest".format(NAMESPACE["opf"]))]
 
-        self.info["spine"] = [{"idref": x.get("idref")} for x in self.opf.find("{0}spine".format(NAMESPACE["opf"]))]
+        self.info["spine"] = [{"idref": x.get("idref")}             # Build a list of spine items
+                              for x in self.opf.find("{0}spine".format(NAMESPACE["opf"]))]
         try:
-            self.info["guide"] = [{"href": x.get("href"),
+            self.info["guide"] = [{"href": x.get("href"),           # Build a list of guide items
                                    "type": x.get("type"),
                                    "title": x.get("title")}
                                   for x in self.opf.find("{0}guide".format(NAMESPACE["opf"]))]
-        except TypeError:
+        except TypeError:                                           # The guide element is optional
             self.info["guide"] = None
 
         # Document identifier
         try:
             self.id = self.opf.find('.//*[@id="{0}"]'.format(self.opf.get("unique-identifier"))).text
         except AttributeError:
-            raise InvalidEpub
-
+            raise InvalidEpub                                       # Cannot process an EPUB without unique-identifier
+                                                                    # attribute of the package element
         # Get and parse the TOC
         toc_id = self.opf[2].get("toc")
         expr = ".//*[@id='{0:s}']".format(toc_id)
         toc_name = self.opf.find(expr).get("href")
         self.ncx_path = self.root_folder + "/" + toc_name
-
         self.ncx = ET.fromstring(self.read(self.ncx_path))
-        self.contents = [{"name": i[0][0].text or "None",
+        self.contents = [{"name": i[0][0].text or "None",           # Build a list of toc elements
                           "src": self.root_folder + "/" + i[1].get("src"),
                           "id":i.get("id")}
-                         for i in self.ncx.iter("{0}navPoint".format(NAMESPACE["ncx"]))]
+                         for i in self.ncx.iter("{0}navPoint".format(NAMESPACE["ncx"]))]    # The iter method
+                                                                                            # loops over nested
+                                                                                            # navPoints
 
     def __init__write(self):
         """
-        Init and empty EPUB
+        Init an empty EPUB
+
         """
         self.opf_path = "OEBPS/content.opf"  # Define a default folder for contents
         self.ncx_path = "OEBPS/toc.ncx"
@@ -150,38 +159,47 @@ class EPUB(ZIP.ZipFile):
         self.opf = ET.fromstring(self._init_opf())  # opf property is always a ElementTree
         self.ncx = ET.fromstring(self._init_ncx())  # so is ncx. Consistent with self.(opf|ncx) built by __init_read()
 
-    def __init__append(self):
-        pass
-
     def close(self):
-        if self.fp is None:  # Check file status
+        if self.fp is None:     # Check file status
             return
-        if self.mode == "r":
+        if self.mode == "r":    # check file mode
             ZIP.ZipFile.close(self)
             return
-        try:
-            global TMP
-            TMP["opf"] = self.opf
-            TMP["ncx"] = self.ncx
-            self._safeclose()
-            ZIP.ZipFile.close(self)
-        except RuntimeError:  # zipfile.__del__ calls close(), ignore
-            return
+        else:
+            try:
+                global TMP                  # in-memory copy of existing opf-ncx. When the epub gets re-init,
+                                            # it loses track of modifications
+                TMP["opf"] = self.opf
+                TMP["ncx"] = self.ncx
+                self._safeclose()
+                ZIP.ZipFile.close(self)     # give back control to superclass close method
+            except RuntimeError:            # zipfile.__del__ destructor calls close(), ignore
+                return
 
     def _safeclose(self):
+        """
+        Preliminary operations before closing an EPUB
+        Writes the empty or modified opf-ncx files before closing the zipfile
+        """
         if self.mode == "a":  # no need to do that if dealing with mode=w files
             self._delete(self.opf_path, self.ncx_path)  # see following horrible hack:
                                                         # zipfile cannot manage overwriting on the archive
                                                         # this basically RECREATES the epub from scratch
                                                         # and is sure slow as hell
+                                                        # ... and a recipe for disaster.
             self.opf = TMP["opf"]
-            self.ncx = TMP["ncx"]  # terribile, but... any better idea?
+            self.ncx = TMP["ncx"]  # get back the temporary copies
 
         self.writestr(self.opf_path, ET.tostring(self.opf, encoding="UTF-8"))
         self.writestr(self.ncx_path, ET.tostring(self.ncx, encoding="UTF-8"))
-        self.__init__read(FLO)  # We still need info dict of a closed EPUB
+        self.__init__read(FLO)  # We may still need info dict of a closed EPUB
 
     def _init_opf(self):
+        """
+        Constructor for empty OPF
+        :type return: xml.minidom.Document
+        :return: xml.minidom.Document
+        """
         today = datetime.date.today()
         opf_tmpl = """<?xml version="1.0" encoding="utf-8" standalone="yes"?>
                         <package xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookId" version="2.0">
@@ -207,6 +225,11 @@ class EPUB(ZIP.ZipFile):
         return doc.toxml('UTF-8')
 
     def _init_ncx(self):
+        """
+        Constructor for empty OPF
+        :type return: xml.minidom.Document
+        :return: xml.minidom.Document
+        """
         ncx_tmpl = """<?xml version="1.0" encoding="utf-8"?>
                         <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
                            "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
@@ -237,8 +260,19 @@ class EPUB(ZIP.ZipFile):
                     </container>"""
         return template % self.opf_path
 
-    def _delete(self, *paths):  # horrible hack
-        global FLO  # this is obviously wrong: any better idea?
+    def _delete(self, *paths):
+        """
+        Delete archive member
+        Basically a hack: zince zipfile can't natively overwrite or delete resources,
+        a new archive is created from scratch to a StringIO file object.
+        The starting file is *never* overwritten.
+        To write the new file to disk, use the writefiletodisk() instance method.
+
+        :type paths: str
+        :param paths: files to be deleted inside EPUB file
+        """
+        global FLO  # File-Like-Object: this is obviously wrong: any better idea?
+                    # Also, the variable name is questionable
         FLO = StringIO()
         new_zip = ZIP.ZipFile(FLO, 'w')
         for item in self.infolist():
@@ -251,18 +285,22 @@ class EPUB(ZIP.ZipFile):
     def additem(self, fileObject, href, mediatype):
         """
         Add a file to manifest only
+
+        :type fileObject: StringIO
+        :param fileObject:
+        :type href: str
+        :param href:
+        :type mediatype: str
+        :param mediatype:
         """
         assert self.mode != "r", "%s is not writable" % self
         element = ET.Element("item", attrib={"id": str(uuid.uuid4())[:5], "href": href, "media-type": mediatype})
         self.writestr(os.path.join(self.root_folder, element.attrib["href"]), fileObject.getvalue())
         self.opf[1].append(element)
 
-    def addpart(self, fileObject, href, mediatype, position=None):
-        """
-        Add a file to manifest, spine and toc
-        :param element: ElementTree.Element
-        """
+    def addpart(self, fileObject, href, mediatype, position=None, type="text"):
         assert self.mode == "w", "%s is not writable" % self
+        # TODO
 
     def writetodisk(self, filename):
         f = open(filename, "w")
