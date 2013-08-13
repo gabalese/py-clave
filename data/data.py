a79efc50438a01b9e42a1f8643cdd6a4aa136651
gabalese <gabriele@alese.it>
2013-08-13 23:11:05 +0200

    tornado.options variable parsing: refactoring of application variables

--- data/data.py
+++ data/data.py
@@ -1,62 +1,28 @@
-import sqlite3
 import os
 
-DBNAME = os.path.join(os.path.dirname(__file__), os.pardir, "base.sql")
-EPUB_FILES_PATH = os.path.join(os.path.dirname(__file__), os.pardir, "files")
+from tornado.options import options
+from tornado.options import define
 
-if not os.path.exists(DBNAME):
-    create = True
+
+dbname = os.path.join(os.path.dirname(__file__), os.pardir, "base.sql")
+epub_path = os.path.join(os.path.dirname(__file__), os.pardir, "files")
+
+define("DBNAME", default=dbname, help="Name of the local DB cache")
+define("PORT", default=8080, help="Port to listen to")
+define("DB_UPDATE_TIMEOUT", default=1000000, help="Interval to update DB")
+define("FEED_UPDATE_TIMEOUT", default=1000000, help="Interval to update XML OPDS feed")
+define("EPUB_FILES_PATH", default=epub_path, help="Files storage directory")
+
+if os.path.exists(os.path.join(__file__, os.pardir, "config.conf")):
+    options.parse_config_file(os.path.join(__file__, os.pardir, "config.conf"))
 else:
-    create = False
+    options.parse_command_line()
 
+for i in options.items():
+    print "{} = {}".format(i[0], i[1])
 
-class DatabaseConnection():
-    def __init__(self, name=DBNAME):
-        self.conn = sqlite3.connect(name)
-        # noinspection PyPropertyAccess
-        self.conn.row_factory = sqlite3.Row
-        self.database = self.conn.cursor()
-
-    def insert(self):
-        pass
-
-    def query(self, field, query):
-        result = self.database.execute("SELECT * FROM books WHERE {} LIKE '%{}%'".format(field, query)).fetchall()
-        return result
-
-    def update(self):
-        pass
-
-    def exit(self):
-        self.conn.commit()
-        self.conn.close()
-
-
-def opendb(name=DBNAME):
-    """
-
-    :param name:
-    :return:
-    """
-    conn = sqlite3.connect(name)
-    # noinspection PyPropertyAccess
-    conn.row_factory = sqlite3.Row
-    database = conn.cursor()
-    return database, conn
-
-if create:
-    print "No suitable DB found, creating one..."
-    database, conn = opendb()
-    database.execute("""
-        CREATE TABLE IF NOT EXISTS
-            books(
-                author text,
-                title text,
-                isbn text UNIQUE,
-                path text PRIMARY KEY,
-                timest timestamp
-                );
-    """)
-    conn.commit()
-    conn.close()
-    print "DB created, with the name of %s" % os.path.basename(DBNAME)
+EPUB_FILES_PATH = options.EPUB_FILES_PATH
+DBNAME = os.path.join(os.path.dirname(__file__), os.pardir, options.DBNAME)
+PORT, DB_UPDATE_TIMEOUT, FEED_UPDATE_TIMEOUT = options.PORT, \
+                                               options.DB_UPDATE_TIMEOUT, \
+                                               options.FEED_UPDATE_TIMEOUT
