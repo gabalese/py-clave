390559176e23ae24379ffb0d8a247ab54e13f008
gabalese <gabriele@alese.it>
2013-08-13 23:09:50 +0200

    New database update function: inserts only new files, removes deleted files.

--- data/utils.py
+++ data/utils.py
@@ -1,14 +1,54 @@
 import os
+import sqlite3
 import time
 
 from multiprocessing import Process, Queue
 
-from data import DBNAME, opendb, EPUB_FILES_PATH
+from data import DBNAME, EPUB_FILES_PATH
 from epub import EPUB, InvalidEpub
+
+
+def initdb():
+    if not os.path.exists(DBNAME):
+        print "No suitable DB found, creating one..."
+        conn = sqlite3.connect(DBNAME)
+        # noinspection PyPropertyAccess
+        conn.row_factory = sqlite3.Row
+        database = conn.cursor()
+        database.execute("""
+            CREATE TABLE IF NOT EXISTS
+                books(
+                    author text,
+                    title text,
+                    isbn text UNIQUE,
+                    path text PRIMARY KEY,
+                    timest timestamp
+                    );
+        """)
+        conn.commit()
+        conn.close()
+        print "DB created, with the name of %s" % os.path.basename(DBNAME)
+    else:
+        return
+
+
+def opendb(name=DBNAME):
+    """
+
+    :param name:
+    :return:
+    """
+    initdb()
+    conn = sqlite3.connect(name)
+    # noinspection PyPropertyAccess
+    conn.row_factory = sqlite3.Row
+    database = conn.cursor()
+    return database, conn
 
 
 def insert_path_indb(queue):
     database, conn = opendb(DBNAME)
+    # noinspection PyPropertyAccess
     conn.text_factory = str
     for path in queue:
         try:
@@ -28,33 +68,100 @@ def insert_path_indb(queue):
         conn.commit()
 
 
-def updateDB(ext="epub"):
-    print "Updating database..."
-    start_time = time.time()
-    path_queue = Queue()
+def delete_path_indb(queue):
+    database, conn = opendb(DBNAME)
+    # noinspection PyPropertyAccess
+    conn.text_factory = str
+    for path in queue:
+        database.execute("DELETE FROM books WHERE path = ?", (path,))
+        conn.commit()
 
-    WORKERS = 4
+
+def updateDB(ext="epub"):
+    print "Begin database update at {}...".format(time.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    start_time = time.time()
+    database, conn = opendb(DBNAME)
+
+    path_queue = Queue()  # path to insert
+    remove_path_queue = Queue()  # path to remove
+
+    dblist = database.execute("SELECT path FROM BOOKS").fetchall()
+    dblist = [str(x[0]) for x in dblist]  # normalize
+
+    walkerlist = []
+
     found = 0
     for path, dirs, files in os.walk(EPUB_FILES_PATH):
         for singular in files:
             if singular.endswith(ext):
                 found += 1
-                filepath = os.path.join(path, singular)
-                path_queue.put(filepath)
+                filepath = os.path.abspath(os.path.join(path, singular))
+                walkerlist.append(filepath)
 
-    processes = []
+    # Analyze
+
+    newfiles = set(walkerlist) - set(dblist)
+    oldfiles = set(dblist) - set(walkerlist)
+
+    for i in newfiles:
+        path_queue.put(i)
+
+    for i in oldfiles:
+        remove_path_queue.put(i)
+
+    WORKERS = 4
+    insert_processes = []
     for i in xrange(WORKERS):
         p = Process(target=insert_path_indb, args=(iter(path_queue.get, None),))
-        p.start()
-        processes.append(p)
         path_queue.put(None)
+        p.start()
+        insert_processes.append(p)
 
-    for p in processes:
+    for p in insert_processes:
         p.join()
 
-    database, conn = opendb(DBNAME)
+    remove_processes = []
+    for i in xrange(WORKERS):
+        p = Process(target=delete_path_indb, args=(iter(remove_path_queue.get, None),))
+        remove_path_queue.put(None)
+        p.start()
+        remove_processes.append(p)
+
+    for p in remove_processes:
+        p.join()
+
     x = database.execute(r"SELECT Count(*) FROM books").fetchone()
-    print "FOUND ", found
-    print "INSERTED ", x[0]
+    print "FOUND: ", found
+    print "NEW: ", len(newfiles)
+    print "OLD: ", len(oldfiles)
+    print "IN DATABASE: ", x[0]
+
+    if found == x[0]:
+        print "OK"
+    else:
+        print "Something went wrong!"
+
     conn.close()
-    print "Update done in {}".format((time.time()-start_time)*1000.00)
+    print "Update done in {} seconds".format(round((time.time()-start_time), 2))
+
+
+class DatabaseConnection():
+    def __init__(self, name=DBNAME):
+        self.conn = sqlite3.connect(name)
+        # noinspection PyPropertyAccess
+        self.conn.row_factory = sqlite3.Row
+        self.database = self.conn.cursor()
+
+    def insert(self):
+        pass
+
+    def query(self, field, query):
+        result = self.database.execute("SELECT * FROM books WHERE {} LIKE '%{}%'".format(field, query)).fetchall()
+        return result
+
+    def update(self):
+        pass
+
+    def exit(self):
+        self.conn.commit()
+        self.conn.close()
